cc63e88cce98d0c74e33ba1ed1962d01556e7b4d
Joshua Richardson <jricjr4@gmail.com>
2011-06-19 14:01:35 -0700

    Added license info.

--- epubtohtml.pl
+++ epubtohtml.pl
@@ -6,6 +6,22 @@
 #
 # * Unzip epub file
 # * do text substitutions as necessary
+# * create a navigation index
+#
+# Copyright 2011 Joshua Richardson, Chegg Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+   
 use Getopt::Long;
 use XML::Simple;
 use Data::Dumper;
